e2167461c651e00cc9a324857f6c539deb13142a
wanglihui <lihui@guanghe.com>
2022-03-01 13:29:17 +0800

    add diffBy

--- slice/diff.go
+++ slice/diff.go
@@ -1,6 +1,6 @@
 package slice
 
-func Diff[T comparable](arr, arr2 []T, f func(a, b T) bool) []T {
+func DiffBy[T comparable](arr, arr2 []T, f func(a, b T) bool) []T {
 	diffArr := make([]T, 0)
 	for _, t := range arr {
 		b := false
@@ -30,4 +30,10 @@ func Diff[T comparable](arr, arr2 []T, f func(a, b T) bool) []T {
 		}
 	}
 	return diffArr
+}
+
+func Diff[T comparable](arr1 []T, arr2 []T) []T {
+	return DiffBy(arr1, arr2, func(a, b T) bool {
+		return a == b
+	})
 }
--- slice/diff__test.go
+++ slice/diff__test.go
@@ -11,9 +11,12 @@ func TestDiff(t *testing.T) {
 	t.Run("diff should be ok", func(t *testing.T) {
 		a1 := []string{"1", "3", "5"}
 		a2 := []string{"5", "7", "9"}
-		diff := slice.Diff(a1, a2, func(a, b string) bool {
+		diff := slice.DiffBy(a1, a2, func(a, b string) bool {
 			return a == b
 		})
+		assert.Equal(t, len(diff), 4)
+		assert.Equal(t, diff[0], a1[0])
+		diff = slice.Diff(a1, a2)
 		assert.Equal(t, len(diff), 4)
 		assert.Equal(t, diff[0], a1[0])
 	})
