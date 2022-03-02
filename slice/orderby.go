6447a2dac9ff3957b42aa42743d52f3e77bf624b
wanglihui <lihui@guanghe.com>
2022-03-02 14:18:06 +0800

    orderby internal func not export

--- slice/orderby.go
+++ slice/orderby.go
@@ -3,29 +3,29 @@ package slice
 type CompareFn[T any] func(left T, right T) int64
 
 func OrderBy[T any](items []T, fn CompareFn[T]) []T {
-	return QuickSort(items, 0, len(items)-1, fn)
+	return quickSort(items, 0, len(items)-1, fn)
 }
-func QuickSort[T any](arr []T, left, right int, fn CompareFn[T]) []T {
+func quickSort[T any](arr []T, left, right int, fn CompareFn[T]) []T {
 	if left < right {
-		PartitionIndex := Partition(arr, left, right, fn)
-		QuickSort(arr, left, PartitionIndex-1, fn)
-		QuickSort(arr, PartitionIndex+1, right, fn)
+		PartitionIndex := partition(arr, left, right, fn)
+		quickSort(arr, left, PartitionIndex-1, fn)
+		quickSort(arr, PartitionIndex+1, right, fn)
 	}
 	return arr
 }
-func Partition[T any](arr []T, left, right int, fn CompareFn[T]) int {
+func partition[T any](arr []T, left, right int, fn CompareFn[T]) int {
 	pivot := left
 	index := pivot + 1
 	for i := index; i <= right; i++ {
 		if fn(arr[i], arr[pivot]) < 0 {
-			Swap(arr, i, index)
+			swap(arr, i, index)
 			index += 1
 		}
 	}
-	Swap(arr, pivot, index-1)
+	swap(arr, pivot, index-1)
 	return index - 1
 }
 
-func Swap[T any](arr []T, i, j int) {
+func swap[T any](arr []T, i, j int) {
 	arr[i], arr[j] = arr[j], arr[i]
 }
